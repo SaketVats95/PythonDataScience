bce2e35e83286e01c6358bb365d76e83b076c84b
Saket Vats <saketvatslpu@gmail.com>
2019-05-30 18:00:24 +0530

    Changed the logic to unzip files so that it first create a folder with same name and the unzip all files into it

--- LogicBuilding/unzipAllFileInsideFolder.py
+++ LogicBuilding/unzipAllFileInsideFolder.py
@@ -1,9 +1,16 @@
 import os,zipfile
 #dir_name = 'D:\\abc'
+
+def RemoveAllZipFile(list_zip):
+	for file_full_name in list_zip:
+		os.remove(file_full_name)
+		print(file_full_name," file deleted as the unzipping completed")
+
+
 dir_name=input("Enter the Directory Name::::")
 dir_name=dir_name.replace('\\','\\')
 extension = ".zip"
-
+list_of_all_zip=[]
 os.chdir(dir_name) # change directory from working dir to dir with files
 try:
 	for path,dir_list,file_list in os.walk(dir_name): # loop through items in dir
@@ -12,14 +19,22 @@ try:
 				file_name_path = os.path.join(path,file_name) # get full path of files
 				print(file_name_path, " file unzipping started")
 				zip_ref = zipfile.ZipFile(file_name_path) # create zipfile object
-				zip_ref.extractall(path) # extract file to dir
+				newDirName=file_name_path.replace(".zip","")
+
+				os.mkdir(newDirName)
+				zip_ref.extractall(newDirName) # extract file to dir
+				#os.listdir(newDirName).__contains__()
+				list_of_all_zip.append(file_name_path)
 				zip_ref.close() # close file
 				print(file_name_path, " file unzipping completed")
+
 except Exception as e:
-	if hasaatr(e,'message'):
+	if hasattr(e,'message'):
 		print(e.message)
 	else:
 		print(e)
 
-        	#os.remove(file_name)
-        	#print(file_name," file deleted as the unzipping completed")
+
+NeedToDelete=input("Enter [Y/N] if you want to delete all zip files:")
+if NeedToDelete.lower()=="y":
+	RemoveAllZipFile(list_of_all_zip)
